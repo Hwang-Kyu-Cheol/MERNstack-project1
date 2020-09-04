96eb93487798f21c6de7d4682ea55c30519c28f2
HKC <iambigman97@naver.com>
2020-09-04 10:40:46 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const port = 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
+const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://HKC:abcd1234@mernstackpractice.1stmk.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log("Mongoose Connected...")).catch((err) => console.log(err));
 
